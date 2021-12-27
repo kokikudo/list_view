de648e16a6254ef6297c7bcd54c1a17465a84df4
kokikudo <massanmesu@icloud.com>
2021-12-27 09:55:53 +0900

    検索機能追加、スクロール時の項目を追加する機能追加

--- app/src/main/java/com/example/list_view/MainActivity.kt
+++ app/src/main/java/com/example/list_view/MainActivity.kt
@@ -6,10 +6,7 @@ import android.util.Log
 import android.view.ActionMode
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.AbsListView
-import android.widget.ArrayAdapter
-import android.widget.CheckedTextView
-import android.widget.Toast
+import android.widget.*
 import androidx.core.view.forEach
 import com.example.list_view.databinding.ActivityMainBinding
 
@@ -25,14 +22,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         // リスト項目データ
-        val data = mutableListOf("もやし", "カルパッチョ", "なす", "まいたけ", "ヨーグルト")
+        val data = mutableListOf(
+            "もやし", "カルパッチョ", "なす", "まいたけ", "ヨーグルト")
 
         // Adapterを定義してリストにセット
-        binding.list.adapter = ArrayAdapter(
+        val adapter = ArrayAdapter(
             this,
             android.R.layout.simple_list_item_checked, // 項目にチェックができるリスト
             data
         )
+        binding.list.adapter = adapter
+
+        // 検索機能用にフィルターを有効化
+        binding.list.isTextFilterEnabled = true
 
         // 長押しすると選択モードに移動するリスナー
         binding.list.setMultiChoiceModeListener(
@@ -82,6 +84,45 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+
+        // リスト末尾まで表示したら次のリストを表示させる
+        binding.list.setOnScrollListener(
+            object : AbsListView.OnScrollListener {
+                override fun onScroll(
+                    view: AbsListView?,
+                    firstVisibleItem: Int,
+                    visibleItemCount: Int,
+                    totalItemCount: Int
+                ) {
+                    if (firstVisibleItem + visibleItemCount + 3 > totalItemCount) {
+                        adapter.add("次　1")
+                        adapter.add("次　2")
+                        adapter.add("次　3")
+                    }
+                }
+
+                override fun onScrollStateChanged(view: AbsListView?, scrollState: Int) {
+                }
+            }
+        )
+
+        // 検索ボックスに入力された時の処理
+        binding.searchView.setOnQueryTextListener(
+            object : SearchView.OnQueryTextListener {
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    if (newText.isNullOrBlank())
+                        binding.list.clearTextFilter()
+                     else
+                        binding.list.setFilterText(newText)
+
+                    return false
+                }
+
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    return false
+                }
+            }
+        )
 
     }
 }
